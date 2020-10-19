48d9f3d5cf2f464e7ffc37480c9082776f05b2eb
lincolnthedev <github@lincolnwilliams.com>
2020-10-19 13:23:05 -0400

    Initial Commit

---
+++ head.php
@@ -0,0 +1,27 @@
+<head>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa&display=swap" rel="stylesheet">
+    <style>
+        body {
+            background-color: #121212;
+            color: #ffffff;
+            font-family: Comfortaa;
+        }
+        a {
+            color: #ffffff;
+        }
+        .mint-nav .active a,
+        .mint-nav .active a:hover {
+            background-color: #5cb85c;
+            color: #ffffff;
+        }
+        .mint-nav a,
+        .mint-nav a:hover {
+            color: #ffffff;
+        }
+    </style>
+</head>
---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+
+//   Head
+
+include "head.php";
+
+//   Nav
+
+echo '
+<ul class="nav nav-pills mint-nav" style="padding-top:1rem;padding-bottom:7rem">
+    <li class="nav-item" style="padding-left:1rem;padding-right:2rem">
+        <h2 class="nav-link">classi</h2>
+    </li>
+    <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="products.php">Products</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+    </li>
+</ul>
+';
+
+
+
+
+
+
+
+//
+//   Page Content
+//
+
+echo '
+<center>
+    <h1 style="font-size:100px"><span class="badge badge-success">New</span> classi</h1>
+        <h3>An all new productivity tool by simple</h3>
+            <br>
+        <a href="" class="btn btn-lg btn-success disabled">Coming Soon</a>
+</center>
+';
+
+
+
+
+
+
+
+//   Footer
+
+echo '
+<center>
+    <footer style="padding-top:12rem;padding-bottom:1rem">
+        <h3>simple</h3>
+        <p>simpleMint UI by @lincolnthedev</p>
+        <!-- <p><a href="legal.php">Legal</a></p> -->
+    </footer>
+</center>
+';
