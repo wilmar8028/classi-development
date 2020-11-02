46f31d9be5c202ce362c6ddc08c4882c60f45e2f
Lincoln W <41193328+lincolnthedev@users.noreply.github.com>
2020-11-02 17:18:17 -0500

    Update gc-api.php

--- gc-api.php
+++ gc-api.php
@@ -13,7 +13,7 @@
 
 //
 //
-// Talking to G Classroom API
+// G Classroom
 //
 //
 
@@ -96,3 +96,46 @@ if (count($results->getCourses()) == 0) {
     printf("%s (%s)\n", $course->getName(), $course->getId());
   }
 }
+
+
+
+
+
+//
+//
+// Google Oauth
+//
+//
+
+<?php
+require_once 'vendor/autoload.php';
+ 
+// init configuration
+$clientID = '<YOUR_CLIENT_ID>';
+$clientSecret = '<YOUR_CLIENT_SECRET>';
+$redirectUri = '<REDIRECT_URI>';
+  
+// create Client Request to access Google API
+$client = new Google_Client();
+$client->setClientId($clientID);
+$client->setClientSecret($clientSecret);
+$client->setRedirectUri($redirectUri);
+$client->addScope("email");
+$client->addScope("profile");
+ 
+// authenticate code from Google OAuth Flow
+if (isset($_GET['code'])) {
+  $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+  $client->setAccessToken($token['access_token']);
+  
+  // get profile info
+  $google_oauth = new Google_Service_Oauth2($client);
+  $google_account_info = $google_oauth->userinfo->get();
+  $email =  $google_account_info->email;
+  $name =  $google_account_info->name;
+ 
+  // now you can use this profile info to create account in your website and make user logged in.
+} else {
+  echo "<a href='".$client->createAuthUrl()."'>Google Login</a>";
+}
+?>
